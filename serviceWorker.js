767cfe5261b82058a3ee183b7427c436079ab47e
jeff93063 <70969969+jeff93063@users.noreply.github.com>
2023-12-17 16:39:27 -0800

    Add files via upload

---
+++ serviceWorker.js
@@ -0,0 +1,33 @@
+const staticCacheName = "units-static-v1";
+const assets = [
+	"/units/",
+	"/units/index.html"
+];
+
+self.addEventListener("install", installEvent => {
+	installEvent.waitUntil(
+		caches.open(staticCacheName).then(cache => {
+			cache.addAll(assets);
+		})
+	);
+});
+
+self.addEventListener('activate', evt => {
+	evt.waitUntil(
+		caches.keys().then(keys =>{
+			//console.log(keys);
+			return Promise.all(keys
+				.filter(key => key !== staticCacheName)
+				.map(key => caches.delete(key))
+			)
+		})
+	);
+});
+
+self.addEventListener("fetch", fetchEvent => {
+	fetchEvent.respondWith(
+		caches.match(fetchEvent.request).then(res => {
+			return res || fetch(fetchEvent.request);
+		})
+	);
+});
